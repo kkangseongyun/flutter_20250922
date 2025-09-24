3ab3c72454e1f8cb08d862647e6cca9e3a54466b
kkang <kkang104@gmail.com>
2025-09-24 13:49:47 +0900

    Add files via upload

--- ch4/stack.dart
+++ ch4/stack.dart
@@ -10,7 +10,59 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Layout Test'),
         ),
-        
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.pink,
+            ),
+            Align(
+              alignment: Alignment(0.0, 0.0),
+              child: Stack(
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/lab_lotte_1.jpg", width: 350,),
+                      Container(
+                        width: 350,
+                        height: 100,
+                        color: Colors.white,
+                        padding: EdgeInsets.only(left: 16, top: 8, bottom: 8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "롯데웨딩위크",
+                              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                            ),
+                            Spacer(),
+                            Text("각 지점 본 매장"),
+                            Text("1.14(금) ~ 1.21(목)")
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                  Positioned(
+                    left: 30,
+                    bottom: 90,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      color: Colors.black,
+                      child: Text(
+                        "111",
+                        style: TextStyle(
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
